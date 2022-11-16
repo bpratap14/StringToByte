f8781001a656873ea9bbdcf91fb684d3098587c8
bpratap14 <118356943+bpratap14@users.noreply.github.com>
2022-11-16 17:28:38 +0530

    Add files via upload

---
+++ controllers/common.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+exports.convertHtmlToBytes = function (req, res, next) {
+    const htmlstring = req.body.htmlString;
+    (async () => {
+
+        // Create a browser instance
+        const browser = await puppeteer.launch();
+      
+        // Create a new page
+        const page = await browser.newPage();
+      
+        //Get HTML content from HTML file
+        //const html = fs.readFileSync('sample.html', 'utf-8');
+        await page.setContent(htmlstring, { waitUntil: 'domcontentloaded' });
+      
+        // To reflect CSS used for screens instead of print
+        await page.emulateMediaType('screen');
+      
+        // Downlaod the PDF
+        const buffer = await page.pdf({    
+          // path:"result.pdf",
+          margin: { top: '50px', right: '50px', bottom: '50px', left: '50px' },
+          printBackground: true,
+          format: 'A4',
+        });
+      
+        // Close the browser instance
+        await browser.close();
+        return res.status(200).json({
+            status: 0,
+            message: "accessgggg.",
+            data:buffer
+          });
+      })();
+    
+  };
